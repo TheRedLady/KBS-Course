ef33c5d176b6f161430613546ca88277e1e2cc53
TheRedLady <christina.hristakieva@gmail.com>
2016-10-11 09:56:17 +0300

    Update puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -1,6 +1,10 @@
 from collections import deque
 
-GOAL = [['X', 1, 2], [3, 4, 5], [6, 7, 8]]
+GOAL = "X12345678"
+
+
+def swap(state, current_pos, new_pos):
+    pass
 
 
 class State(object):
@@ -11,20 +15,41 @@ class State(object):
         self.parent = parent
 
     def is_goal(self):
+        # return goal.distance == 0
         return self.state == GOAL
 
     def index_of_x(self):
         for i in range(0, len(self.state)):
             if self.state[i] == 'X':
-                return i/3, i%3
+                return i
 
-    def is_valid_state(self, index):
+    @staticmethod
+    def is_valid_state(index):
         return index[0] in (0, 1, 2) and index[1] in (0, 1, 2)
 
     def generate_children(self):
         x_row, x_col = self.index_of_x()
         moves = [(x_row + 1, x_col), (x_row, x_col + 1), (x_row - 1, x_col), (x_row, x_col - 1)]
         filter(State.is_valid_state, moves)
+        return [State(swap(self.state(x_row, x_col), move))for move in mooves]
+
+    def compare(self, other_state):
+        return self.distance - other_state.distance
+
+    def get_position(self, element):
+        for i in range(0, len(self.state)):
+            if self.state[i] == element:
+                return i
+
+    @property
+    def distance(self):
+        distance = 0
+        for i in range(0, len(GOAL)):
+            pos = self.get_position(GOAL[i])
+            x = (i % 3) - (pos % 3)
+            y = (i / 3) - (pos / 3)
+            distance += x + y
+        return distance
 
     def get_children(self):
         if not self.children:
@@ -32,6 +57,7 @@ class State(object):
         return self.children
 
     def solve(self):
+        # use priority queue for weight
         queue = deque()
         queue.append(self.state)
         while not queue.isEmpty():
