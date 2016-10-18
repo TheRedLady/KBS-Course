e0b150c699af3b0f7118c9769575f8b9ce3dd2f7
TheRedLady <christina.hristakieva@gmail.com>
2016-10-18 10:02:26 +0300

    Update mitko

--- helpmitko2016.py
+++ helpmitko2016.py
@@ -25,6 +25,13 @@ class Node(object):
             self.children.extend([Node(move) for move in filter(Node.is_valid, moves)])
         return self.children
 
+    def get_cost(self):
+        # if self.state.x == child.state.x || self.state.y == child.state.y: cost is 1
+        # check terrain
+        # if move is water : cost is 2
+        # if diagonal move: cost is 1.5
+        pass
+
     @staticmethod
     def get_distance(state):
         '''
@@ -34,14 +41,25 @@ class Node(object):
         x_distance = abs(GOAL[0] - state[0])
         y_distance = abs(GOAL[1] - state[1])
         return x_distance + y_distance
+        # abs(x_distance, y_distance) + sqrt(2 * min(x_distance, y_distance)^2) * 1,5
 
     def solve(self, start):
+        visited = set()
         start_node = Node(start)
         queue = deque()
+        # queue.push(start_node.get_cost + start_node.get_distance)
         queue.push(start_node)
         while queue:
             current = queue.popleft()
+            visited.add(current)
             if not current.get_distance():
                 return current
+            if current in visited:
+                continue
+            # open and closed for nodes
+            # algorithm not full
+            # second option: filter open children
+            # third option: queue contains pairs of node and priority/cost
             queue.extend(current.get_next())
+            # third option: key node: node.get_cost + node.get_distance
             queue = sorted(key=lambda node: node.get_distance())
